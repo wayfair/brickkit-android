22fee2871b1cdca1e67b9322cdc9d50d77b8d2ba
Richard Steventon <rsteventon1@wayfair.com>
2019-09-03 15:05:14 -0400

    Fix NPE.

--- BrickKit/bricks/src/main/java/com/wayfair/brickkit/behavior/StickyViewBehavior.java
+++ BrickKit/bricks/src/main/java/com/wayfair/brickkit/behavior/StickyViewBehavior.java
@@ -100,6 +100,10 @@ abstract class StickyViewBehavior extends BrickBehavior {
 
     @Override
     public void onScroll() {
+        if (brickDataManager == null || brickDataManager.getBrickRecyclerAdapter() == null) {
+            Log.w(this.getClass().getSimpleName(), "Behavior attached or onScroll called but the Manager or Adapter is null");
+            return;
+        }
         BrickRecyclerAdapter adapter = brickDataManager.getBrickRecyclerAdapter();
         if (stickyHolderContainer == null && adapter.getRecyclerView() != null && brickDataManager.getRecyclerViewParent() != null) {
             stickyHolderContainer = (ViewGroup) (brickDataManager.getRecyclerViewParent()).findViewById(stickyViewContainerId);
@@ -131,9 +135,8 @@ abstract class StickyViewBehavior extends BrickBehavior {
     protected boolean attach(RecyclerView recyclerView) {
         if (recyclerView != null) {
             recyclerView.addOnScrollListener(this);
-            recyclerView.post(new Runnable() {
-                @Override
-                public void run() {
+            recyclerView.post(() -> {
+                if (StickyViewBehavior.this != null) {
                     onScroll();
                 }
             });
