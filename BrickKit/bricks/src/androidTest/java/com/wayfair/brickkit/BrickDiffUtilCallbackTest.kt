d1fd91a25a84afcfc2a97de2e78bce85096346e1
Tom Husson <thusson13@gmail.com>
2021-01-13 12:43:04 -0500

    Convert BrickDiffUtilCallback to Kotlin (and add tests (#175)
    Fixes https://github.com/wayfair/brickkit-android/issues/174

---
+++ BrickKit/bricks/src/androidTest/java/com/wayfair/brickkit/BrickDiffUtilCallbackTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2017-2021 Wayfair. All rights reserved.
+ */
+package com.wayfair.brickkit
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.wayfair.brickkit.brick.BaseBrick
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class BrickDiffUtilCallbackTest {
+    private lateinit var callback: BrickDiffUtilCallback
+
+    @Before
+    fun setup() {
+        val brick1 = mock<BaseBrick>()
+        whenever(brick1.layout).thenReturn(LAYOUT_1)
+
+        val brick2 = mock<BaseBrick>()
+        whenever(brick2.layout).thenReturn(LAYOUT_2)
+
+        callback = BrickDiffUtilCallback(listOf(brick1), listOf(brick1, brick2))
+    }
+
+    @Test
+    fun testGetOldListSize() {
+        assertEquals(1, callback.oldListSize)
+    }
+
+    @Test
+    fun testGetNewListSize() {
+        assertEquals(2, callback.newListSize)
+    }
+
+    @Test
+    fun testAreItemsTheSame_same() {
+        assertTrue(callback.areItemsTheSame(0, 0))
+    }
+
+    @Test
+    fun testAreItemsTheSame_different() {
+        assertFalse(callback.areItemsTheSame(0, 1))
+    }
+
+    @Test
+    fun testAreContentsTheSame_same() {
+        assertTrue(callback.areContentsTheSame(0, 0))
+    }
+
+    @Test
+    fun testAreContentsTheSame_different() {
+        assertFalse(callback.areContentsTheSame(0, 1))
+    }
+
+    companion object {
+        private const val LAYOUT_1 = 1
+        private const val LAYOUT_2 = 2
+    }
+}
