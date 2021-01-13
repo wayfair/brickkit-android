1f62805dbe5d0835703964dfbb3424127d62d7ab
Tom Husson <thusson13@gmail.com>
2021-01-13 12:01:19 -0500

    Convert BrickDiffUtilCallback to Kotlin (and add tests
    Fixes https://github.com/wayfair/brickkit-android/issues/174

---
+++ BrickKit/bricks/src/main/java/com/wayfair/brickkit/BrickDiffUtilCallback.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2017-2021 Wayfair. All rights reserved.
+ */
+package com.wayfair.brickkit
+
+import androidx.recyclerview.widget.DiffUtil
+import com.wayfair.brickkit.brick.BaseBrick
+
+/**
+ * A DiffUtil for Bricks.
+ */
+class BrickDiffUtilCallback internal constructor(
+    private val oldList: List<BaseBrick>,
+    private val newList: List<BaseBrick>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition].layout == newList[newItemPosition].layout
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition] == newList[newItemPosition]
+}
