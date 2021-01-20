1b023e94b6223674cf849ed1cf98901a1a5b0a4f
Tom Husson <thusson13@gmail.com>
2021-01-20 09:35:30 -0500

    Update AvoidFlickerItemAnimator to only set values on creation
    Fixes https://github.com/wayfair/brickkit-android/issues/185

--- BrickKit/bricks/src/main/java/com/wayfair/brickkit/animator/AvoidFlickerItemAnimator.kt
+++ BrickKit/bricks/src/main/java/com/wayfair/brickkit/animator/AvoidFlickerItemAnimator.kt
@@ -1,19 +1,10 @@
 package com.wayfair.brickkit.animator
 
 import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.RecyclerView
 
 internal class AvoidFlickerItemAnimator : DefaultItemAnimator() {
-    override fun animateChange(
-        oldHolder: RecyclerView.ViewHolder,
-        newHolder: RecyclerView.ViewHolder,
-        fromX: Int,
-        fromY: Int,
-        toX: Int,
-        toY: Int
-    ): Boolean {
+    init {
         changeDuration = 0
         supportsChangeAnimations = false
-        return super.animateChange(oldHolder, newHolder, fromX, fromY, toX, toY)
     }
 }
