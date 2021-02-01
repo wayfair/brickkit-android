5a6503494c39ed45efad996c9a54a89d4394873f
Tom Husson <thusson13@gmail.com>
2021-02-01 16:14:05 -0500

    Convert FragmentBrickFragment to kotlin
    Fixes https://github.com/wayfair/brickkit-android/issues/206

---
+++ BrickKit/app/src/main/java/com/wayfair/brickkitdemo/fragmentbrick/FragmentBrickFragment.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2017-2021 Wayfair. All rights reserved.
+ */
+package com.wayfair.brickkitdemo.fragmentbrick
+
+import android.os.Bundle
+import com.wayfair.brickkitdemo.BrickFragment
+import com.wayfair.brickkitdemo.R
+import com.wayfair.brickkitdemo.SimpleBrickFragment
+import com.wayfair.brickkitdemo.fragmentbrick.bricks.FragmentBrick
+
+/**
+ * Example of fragment containing [FragmentBrick]'s containing [SimpleBrickFragment]'s.
+ */
+class FragmentBrickFragment : BrickFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        FragmentBrick(childFragmentManager, SimpleBrickFragment.newInstance(1), R.color.colorAccent).addLastTo(dataManager)
+        FragmentBrick(childFragmentManager, SimpleBrickFragment.newInstance(2), R.color.colorPrimary).addLastTo(dataManager)
+        FragmentBrick(childFragmentManager, SimpleBrickFragment.newInstance(3), R.color.colorPrimaryDark).addLastTo(dataManager)
+    }
+}
